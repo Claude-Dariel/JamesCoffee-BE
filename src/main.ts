9b5fd9cfd1a72fc549ad8741aee01baf468fdc7a
sizwetshabangu2024 <162637131+sizwetshabangu2024@users.noreply.github.com>
2024-04-29 18:46:41 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  let port = process.env.PORT || 8000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8001);
+  await app.listen(port);
 }
 bootstrap();
